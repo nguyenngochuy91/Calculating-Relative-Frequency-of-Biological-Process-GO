d51b427be01cb364b7ff652dc3a6699974357cf9
Huy Nguyen <huyn@iastate.edu>
2016-09-27 11:35:21 -0500

    Using different feature
    Instead of reading from the operon DB, parse from the reference gb file to get the gene and the GI ID. I made this change so it can work on other genome (E.Coli etc.)

--- get_genes_name.py
+++ get_genes_name.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 ''' Author  : Huy Nguyen
-    Program : For each B.Sub operon, get its gene (in bsu-BSU format)
+    Program : For each E Coli operon, get its gene (in bsu-BSU format)
     Start   : 08/18/2016
     End     : 08/20/2016
 '''
 import argparse
-import os
+from Bio import SeqIO
+synonym_dic ={'rapZ':'yhbJ','cas1':'ygbT','cas2':'ygbF','yjeE':'tsaE',
+              'gutM':'srlM','gutQ':'srlQ','yjeF':'nnr'}
 # get the arguments from command line
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--Operon","-i",help="ProOperon dp file")
-    parser.add_argument("--GeneBlock","-g", help="gene blocks that went through filtered")
-    parser.add_argument("--Outfile","-o", help="File out for the new result dic")
-    parser.add_argument("--GeneName","-n", help="Gene name to parse into uniprot to get protein file")
+    # parser.add_argument("--Operon","-i",help="ProOperon dp file")
+    parser.add_argument("--GeneBlock","-g", help="gene blocks that went through filtered 'gene_block_names_and_genes.txt'")
+    parser.add_argument("--Outfile","-o", help="File out for the new result dic 'operons_genes.txt'")
+    parser.add_argument("--GeneName","-n", help="Gene name to parse into uniprot to get protein file 'name_ecocyc_uniprot.txt'")
+    parser.add_argument("--Reference","-r", help="Reference genome 'NC_000913.gbk'")
     args = parser.parse_args()
     return args
 ###############################################################################
@@ -27,18 +30,21 @@ def parse_gene_block_names_and_genes(myfile):
         items = line.split('\t')
         # print items
         mydic[items[0]] = items[1:]
+
     return mydic
-# parse the B.Subtilis operon txt file
-def parse_operon(myfile):
-    infile = open(myfile,'r')
+
+# parse the NC_000913.gbk file
+def parse_reference(myfile):
     mydic={}
-    # ignore the first line
-    for line in infile.readlines()[0:]:
-        line = line.split('\t')
-        mydic[line[1]] = line[2]
+    record = SeqIO.read(myfile,"genbank")
+    for feature in record.features:
+        if feature.type == 'CDS':
+            for item in feature.qualifiers['db_xref']:
+                if item[:2]=='GI':
+                    # print feature
+                    mydic[feature.qualifiers['gene'][0]] = item[3:]
+
     return mydic
-
-
 # using the 2 dictionary to write out new operon file, where each line starts with 
 # starter gene name, follow by what gene in the operon in bsu-BSU format
 # in addition, also wrote out gene names in following format BSUB:BSU32510-MONOMER
@@ -49,20 +55,29 @@ def return_dic_bsu(dic1,dic2,outfile,GeneName):
         # initiate a list
         out.write(item)
         for gene in dic1[item]: # iterate through gene annotation
-            name.write('BSUB:'+dic2[gene]+'-MONOMER'+'\n')
-            out.write('\t'+dic2[gene])
+            if gene in synonym_dic:
+                gene = synonym_dic[gene]
+            try:
+                name.write(dic2[gene]+'\n')
+                out.write('\t'+dic2[gene])
+            except:      
+                print gene
+                out.write('\t'+dic2[gene])
         out.write('\n')
     out.close()
     
     
 if __name__ == "__main__":
     args       = get_arguments()
-    Operon     = args.Operon
+    # Operon     = args.Operon
     GeneBlock  = args.GeneBlock
     GeneName   = args.GeneName
     Outfile    = args.Outfile
-    operon_dic = parse_operon(Operon)
+    Reference  = args.Reference
+    uniprot_dic = parse_reference(Reference)
+    # print 'uniprot_dic',uniprot_dic
+    # operon_dic = parse_operon(Operon)
     geneBlock_dic = parse_gene_block_names_and_genes(GeneBlock)
-    return_dic_bsu(geneBlock_dic,operon_dic,Outfile,GeneName)
-
+    # print 'geneBlock_dic',geneBlock_dic
+    return_dic_bsu(geneBlock_dic,uniprot_dic,Outfile,GeneName)
     
