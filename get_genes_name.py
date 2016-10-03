c344839e2191bbd52cec306208fa4504256cb634
Huy Nguyen <huyn@iastate.edu>
2016-10-03 15:22:14 -0500

    Update get_genes_name.py

--- get_genes_name.py
+++ get_genes_name.py
@@ -14,8 +14,9 @@ def get_arguments():
     # parser.add_argument("--Operon","-i",help="ProOperon dp file")
     parser.add_argument("--GeneBlock","-g", help="gene blocks that went through filtered 'gene_block_names_and_genes.txt'")
     parser.add_argument("--Outfile","-o", help="File out for the new result dic 'operons_genes.txt'")
-    parser.add_argument("--GeneName","-n", help="Gene name to parse into uniprot to get protein file 'name_ecocyc_uniprot.txt'")
-    parser.add_argument("--Reference","-r", help="Reference genome 'NC_000913.gbk'")
+    parser.add_argument("--GeneName","-n", help="Gene name to parse into uniprot to get protein file 'name_bsucyc_uniprot.txt'")
+    parser.add_argument("--Reference","-r", help="Reference genome 'NC_000964.gbk'")
+    parser.add_argument("--GI_locus","-l", help="Mapping between GI number and the locus in ase can't find by GI (GI_locus.txt)")
     args = parser.parse_args()
     return args
 ###############################################################################
@@ -32,40 +33,48 @@ def parse_gene_block_names_and_genes(myfile):
         mydic[items[0]] = items[1:]
 
     return mydic
+    
+    
 
 # parse the NC_000913.gbk file
 def parse_reference(myfile):
-    mydic={}
+    GI_dic={}
+    locus_dic ={}
     record = SeqIO.read(myfile,"genbank")
     for feature in record.features:
         if feature.type == 'CDS':
+            # need the locus tag to query from the downloading file (name is different)
+            locus_dic[feature.qualifiers['gene'][0]] = feature.qualifiers['locus_tag'][0]
             for item in feature.qualifiers['db_xref']:
                 if item[:2]=='GI':
-                    # print feature
-                    mydic[feature.qualifiers['gene'][0]] = item[3:]
+                    # get the GI number to find from uniprot
+                    GI_dic[feature.qualifiers['gene'][0]] = item[3:]
+                
 
-    return mydic
+    return GI_dic,locus_dic
 # using the 2 dictionary to write out new operon file, where each line starts with 
 # starter gene name, follow by what gene in the operon in bsu-BSU format
 # in addition, also wrote out gene names in following format BSUB:BSU32510-MONOMER
-def return_dic_bsu(dic1,dic2,outfile,GeneName):
-    out= open(outfile,'w')
-    name = open(GeneName,'w')
-    for item in dic1:
+def return_dic_bsu(geneBlock_dic,GI_dic,locus_dic,outfile,GeneName,GI_locus):
+    outfile_name= open(outfile,'w')
+    GeneName_file = open(GeneName,'w')
+    GI_locus_file = open(GI_locus,'w')
+    for item in geneBlock_dic:
         # initiate a list
-        out.write(item)
-        for gene in dic1[item]: # iterate through gene annotation
+        outfile_name.write(item)
+        for gene in geneBlock_dic[item]: # iterate through gene annotation
             if gene in synonym_dic:
                 gene = synonym_dic[gene]
             try:
-                name.write(dic2[gene]+'\n')
-                out.write('\t'+dic2[gene])
+                GeneName_file.write(GI_dic[gene]+'\n')
+                outfile_name.write('\t'+locus_dic[gene])
+                GI_locus_file.write(GI_dic[gene]+'\t'+locus_dic[gene]+'\n')
             except:      
                 print gene
-                out.write('\t'+dic2[gene])
-        out.write('\n')
-    out.close()
-    
+        outfile_name.write('\n')
+    outfile_name.close()
+    GeneName_file.close()
+    GI_locus_file.close()
     
 if __name__ == "__main__":
     args       = get_arguments()
@@ -74,10 +83,11 @@ if __name__ == "__main__":
     GeneName   = args.GeneName
     Outfile    = args.Outfile
     Reference  = args.Reference
-    uniprot_dic = parse_reference(Reference)
+    GI_locus   = args.GI_locus
+    GI_dic,locus_dic = parse_reference(Reference)
     # print 'uniprot_dic',uniprot_dic
     # operon_dic = parse_operon(Operon)
     geneBlock_dic = parse_gene_block_names_and_genes(GeneBlock)
     # print 'geneBlock_dic',geneBlock_dic
-    return_dic_bsu(geneBlock_dic,uniprot_dic,Outfile,GeneName)
+    return_dic_bsu(geneBlock_dic,GI_dic,locus_dic,Outfile,GeneName,GI_locus)
     
